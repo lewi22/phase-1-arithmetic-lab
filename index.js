c08966be71ec07c8c0246928f9577ebe28019b3a
lewinsky <lewinskygatwirim@gmail.com>
2022-06-12 07:03:52 -0400

    commit tree 66267bc916f2030990080e863057bb1e75d784ae

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+// Write your const num1 = 31
+const num2 = 2;
+
+const multiply = num1 * num2
+
+
+const random = Math.floor(Math.random() * 2) ;
+
+const num3 = 29;
+
+const num4 = 5;
+
+const mod= num3%num4;
+
+console.log (mod)
+const max = Math.max(1,2,3,4,5,6,7,8,9,10,20) ;
+console.log (max) ;
+
